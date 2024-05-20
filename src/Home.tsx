f6f886fd64fc16655a0bf15b8fcc9358d89e9772
merukode <auliarama965@gmail.com>
2024-05-20 14:44:31 +0700

    Update Package

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,12 +1,22 @@
-import { View, Text } from 'react-native'
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <ScrollView style={styles.root}>
+      <View>
+        <Text>Find Your Favourite Food</Text>
+        <Ionicons name='notifications-circle-outline' size={40}/>
+      </View>
+    </ScrollView>
   )
 }
 
 export default Home
+
+const styles = StyleSheet.create({
+  root: {
+    backgroundColor: "#2A2C38",
+  }
+})
